235a8c1974cf6a5aa4a6c1bcbbfed0a6f64d9d9d
Amrit Shenava <amritshenava@outlook.com>
2019-05-30 12:53:22 -0400

    Adding files

---
+++ hackernews/src/Atr.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function Atr(){
+  return(
+    <div className="atr">
+      <div className="atrelements">
+        <h1>Amrth Ashok Shenava</h1>
+        <h2>Student</h2>
+        <h3>I am interested in Blockchain, AI, Robotics and am looking forward
+        to learn more and work on some interesting porjects.</h3>
+      </div>
+      <div className="button">
+        <button>Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default Atr;
